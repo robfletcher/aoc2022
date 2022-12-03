424c251d442bbbbaed4f47dab6ef1ea0d8c70efb
Rob Fletcher <rfletcher@netflix.com>
2022-12-03 11:29:01 -0800

    day 3 part 2 solution

---
+++ src/main/kotlin/day3.kt
@@ -0,0 +1,38 @@
+import java.io.File
+
+val values = listOf(' ') + ('a'..'z') + ('A'..'Z')
+
+fun main() {
+  part1()
+  part2()
+}
+
+fun part1() {
+  File("inputs/day3")
+    .reader()
+    .useLines { lines ->
+      lines.fold(0) { total, line ->
+        val (a, b) = line.halve()
+        total + a.first { it in b }.let(values::indexOf)
+      }
+    }
+    .also(::println)
+    .also { assert(it == 7817) }
+}
+
+fun String.halve() =
+  (length / 2).let { mid ->
+    slice(0 until mid) to slice(mid until length)
+  }
+
+fun part2() {
+  File("inputs/day3")
+    .reader()
+    .useLines { lines ->
+      lines.chunked(3).fold(0) { total, (l1, l2, l3) ->
+        total + l1.first { it in l2 && it in l3 }.let(values::indexOf)
+      }
+    }
+    .also(::println)
+    .also { assert(it == 2444) }
+}
