abef9425b3b092c5873e19c64346737ead58f146
Rob Fletcher <rfletcher@netflix.com>
2022-12-04 09:52:48 -0800

    day 4 part 2

--- day4/src/main/kotlin/main.kt
+++ day4/src/main/kotlin/main.kt
@@ -9,9 +9,7 @@ fun part1() {
           .split(',', limit = 2)
           .map(String::parseRange)
           .sortedByDescending { it.size }
-          .let { (larger, smaller) ->
-            larger.containsAll(smaller)
-          }
+          .let { (larger, smaller) -> larger.containsAll(smaller) }
       }
     }
     .also(::println)
@@ -30,6 +28,25 @@ fun IntRange.containsAll(other: IntRange) =
 val IntRange.size
   get() = (endInclusive - start) + 1
 
+fun part2() {
+  File("input")
+    .reader()
+    .useLines { lines ->
+      lines.count { line ->
+        line
+          .split(',', limit = 2)
+          .map(String::parseRange)
+          .let { (a, b) -> a.overlaps(b) }
+      }
+    }
+    .also(::println)
+    .also { assert(it == 897) }
+}
+
+fun IntRange.overlaps(other: IntRange) =
+  first <= other.last && last >= other.first
+
 fun main() {
   part1()
+  part2()
 }
