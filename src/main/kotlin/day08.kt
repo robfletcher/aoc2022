ec09020cbf5bee81440e293c929ef15c4d89f1ec
Rob Fletcher <rfletcher@netflix.com>
2022-12-07 23:17:41 -0800

    day 8 part 2

--- src/main/kotlin/day08.kt
+++ src/main/kotlin/day08.kt
@@ -2,24 +2,41 @@ import java.io.Reader
 
 data class Coordinate(val x: Int, val y: Int)
 
-@JvmInline
-value class Tree(val height: Int)
-
 fun main() {
-  fun part1(input: Reader): Int {
-    val map = mutableMapOf<Coordinate, Tree>()
+  fun mapTrees(input: Reader): Map<Coordinate, Int> {
+    val map = mutableMapOf<Coordinate, Int>()
     input.useLines { lines ->
       lines.forEachIndexed { y, line ->
         line.forEachIndexed { x, c ->
-          map[Coordinate(x, y)] = Tree(c.digitToInt())
+          map[Coordinate(x, y)] = c.digitToInt()
         }
       }
     }
-    return map.count { (coordinate, tree) ->
-      map.none { it.key.x == coordinate.x && it.key.y < coordinate.y && it.value.height >= tree.height }
-        || map.none { it.key.x == coordinate.x && it.key.y > coordinate.y && it.value.height >= tree.height }
-        || map.none { it.key.x < coordinate.x && it.key.y == coordinate.y && it.value.height >= tree.height }
-        || map.none { it.key.x > coordinate.x && it.key.y == coordinate.y && it.value.height >= tree.height }
+    return map
+  }
+
+  fun part1(input: Reader): Int {
+    val map = mapTrees(input)
+    return map.count { (coordinate, height) ->
+      map.none { it.key.x == coordinate.x && it.key.y < coordinate.y && it.value >= height }
+        || map.none { it.key.x == coordinate.x && it.key.y > coordinate.y && it.value >= height }
+        || map.none { it.key.x < coordinate.x && it.key.y == coordinate.y && it.value >= height }
+        || map.none { it.key.x > coordinate.x && it.key.y == coordinate.y && it.value >= height }
+    }
+  }
+
+  fun part2(input: Reader): Int {
+    val map = mapTrees(input)
+    return map.maxOf { (coordinate, height) ->
+      val up = map.keys.filter { it.x == coordinate.x && it.y < coordinate.y }.sortedByDescending { it.y }
+        .run { indexOfFirst { map.getValue(it) >= height }.let { if (it < 0) size else it + 1 } }
+      val left = map.keys.filter { it.x < coordinate.x && it.y == coordinate.y }.sortedByDescending { it.x }
+        .run { indexOfFirst { map.getValue(it) >= height }.let { if (it < 0) size else it + 1 } }
+      val down = map.keys.filter { it.x == coordinate.x && it.y > coordinate.y }.sortedBy { it.y }
+        .run { indexOfFirst { map.getValue(it) >= height }.let { if (it < 0) size else it + 1 } }
+      val right = map.keys.filter { it.x > coordinate.x && it.y == coordinate.y }.sortedBy { it.x }
+        .run { indexOfFirst { map.getValue(it) >= height }.let { if (it < 0) size else it + 1 } }
+      left * right * up * down
     }
   }
 
@@ -31,7 +48,9 @@ fun main() {
     35390
   """.trimIndent()
   assert(part1(testInput.reader()) == 21)
+  assert(part2(testInput.reader()) == 8)
 
   val input = readInput("day08")
   part1(input()).also(::println)
+  part2(input()).also(::println)
 }
