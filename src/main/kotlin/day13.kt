ed8bda6cf718322d655a258a58cc4dc6faff7dbb
Rob Fletcher <rfletcher@netflix.com>
2022-12-14 02:30:44 +0000

    day 13 part 2

--- src/main/kotlin/day13.kt
+++ src/main/kotlin/day13.kt
@@ -10,49 +10,62 @@ fun main() {
       val itr1 = o1.iterator()
       val itr2 = o2.iterator()
       while (itr1.hasNext() && itr2.hasNext()) {
-        val element1 = itr1.next()
-        val element2 = itr2.next()
-        if (element1 is JsonPrimitive && element2 is JsonPrimitive) {
-          if (element1.int < element2.int) return -1
-          else if (element1.int > element2.int) return 1
-        } else if (element1 is JsonArray && element2 is JsonArray) {
-          val comparison = compare(element1, element2)
-          if (comparison != 0) return comparison
-        } else if (element1 is JsonArray && element2 is JsonPrimitive) {
-          val comparison = compare(element1, JsonArray(listOf(element2)))
-          if (comparison != 0) return comparison
-        } else if (element1 is JsonPrimitive && element2 is JsonArray) {
-          val comparison = compare(JsonArray(listOf(element1)), element2)
-          if (comparison != 0) return comparison
+        val e1 = itr1.next()
+        val e2 = itr2.next()
+        if (e1 is JsonPrimitive && e2 is JsonPrimitive) {
+          if (e1.int < e2.int) return -1
+          else if (e1.int > e2.int) return 1
+        } else {
+          val delta = compare(
+            e1 as? JsonArray ?: JsonArray(listOf(e1)),
+            e2 as? JsonArray ?: JsonArray(listOf(e2))
+          )
+          if (delta != 0) return delta
         }
       }
-
-      return if (!itr1.hasNext()) -1 else if (!itr2.hasNext()) 1 else 0
+      return when {
+        itr1.hasNext() && !itr2.hasNext() -> 1
+        !itr1.hasNext() && itr2.hasNext() -> -1
+        else -> 0
+      }
     }
   }
 
-  fun parse(packet: String) = Json.parseToJsonElement(packet) as JsonArray
+  fun String.packet() = Json.parseToJsonElement(this) as JsonArray
 
   fun part1(reader: Reader): Int {
     val matchingIndices = mutableListOf<Int>()
     reader.useLines { lines ->
       lines.chunked(3).forEachIndexed { index, (a, b) ->
-        if (comparator.compare(parse(a), parse(b)) <= 0) {
+        if (comparator.compare(a.packet(), b.packet()) <= 0) {
           matchingIndices.add(index + 1)
         }
       }
     }
-    return matchingIndices.also(::println).sum()
+    return matchingIndices.sum()
   }
 
-  assert(comparator.compare(parse("[1,1,3,1,1]"), parse("[1,1,5,1,1]")) < 0)
-  assert(comparator.compare(parse("[[1],[2,3,4]]"), parse("[[1],4]")) < 0)
-  assert(comparator.compare(parse("[9]"), parse("[[8,7,6]]")) > 0)
-  assert(comparator.compare(parse("[[4,4],4,4]"), parse("[[4,4],4,4,4]")) < 0)
-  assert(comparator.compare(parse("[7,7,7,7]"), parse("[7,7,7]")) > 0)
-  assert(comparator.compare(parse("[]"), parse("[3]")) < 0)
-  assert(comparator.compare(parse("[[[]]]"), parse("[[]]")) > 0)
-  assert(comparator.compare(parse("[1,[2,[3,[4,[5,6,7]]]],8,9]"), parse("[1,[2,[3,[4,[5,6,0]]]],8,9]")) > 0)
+  fun part2(reader: Reader): Int {
+    val m1: JsonArray = "[[2]]".packet()
+    val m2: JsonArray = "[[6]]".packet()
+    return reader.useLines { lines ->
+      lines.mapTo(mutableListOf(m1, m2)) { if (it.isBlank()) null else it.packet() }
+    }
+      .filterNotNull()
+      .sortedWith(comparator)
+      .run {
+        (indexOf(m1) + 1) * (indexOf(m2) + 1)
+      }
+  }
+
+  assert(comparator.compare("[1,1,3,1,1]".packet(), "[1,1,5,1,1]".packet()) < 0)
+  assert(comparator.compare("[[1],[2,3,4]]".packet(), "[[1],4]".packet()) < 0)
+  assert(comparator.compare("[9]".packet(), "[[8,7,6]]".packet()) > 0)
+  assert(comparator.compare("[[4,4],4,4]".packet(), "[[4,4],4,4,4]".packet()) < 0)
+  assert(comparator.compare("[7,7,7,7]".packet(), "[7,7,7]".packet()) > 0)
+  assert(comparator.compare("[]".packet(), "[3]".packet()) < 0)
+  assert(comparator.compare("[[[]]]".packet(), "[[]]".packet()) > 0)
+  assert(comparator.compare("[1,[2,[3,[4,[5,6,7]]]],8,9]".packet(), "[1,[2,[3,[4,[5,6,0]]]],8,9]".packet()) > 0)
 
   val testInput = """
     [1,1,3,1,1]
@@ -81,7 +94,9 @@ fun main() {
   """.trimIndent()
 
   assert(part1(testInput.reader()) == 13)
+  assert(part2(testInput.reader()) == 140)
 
   val input = readInput("day13")
-  part1(input()).also(::println).also { assert(it < 5532) }
+  part1(input()).also(::println).also { assert(it == 5196) }
+  part2(input()).also(::println).also { assert(it == 22134) }
 }
