1c18b61b7d6fe175f8cf7e167ccb34b54c49e5dc
Rob Fletcher <rfletcher@netflix.com>
2022-12-15 21:22:27 +0000

    day 15 part 2 working fast enough to be usable, but I bet it can be faster

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -8,20 +8,27 @@ data class Sensor(
   val location: Coordinate,
   val closestBeacon: Coordinate
 ) {
-  val beaconDistance by lazy { location.manhattanDistanceFrom(closestBeacon) }
+  private val beaconDistance by lazy { location.manhattanDistanceFrom(closestBeacon) }
 
-  private val exclusionZone: MutableMap<Int, IntRange> = mutableMapOf()
-  fun columnsWithNoBeacons(row: Int): IntRange =
-    exclusionZone.computeIfAbsent(row) { y ->
-      (beaconDistance - abs(location.y - y)).let { x ->
-        (location.x - x)..(location.x + x)
-      }
+  fun columnsWithNoBeacons(row: Int) =
+    (beaconDistance - abs(location.y - row)).let { x ->
+      (location.x - x)..(location.x + x)
     }
 }
 
-fun Iterable<Sensor>.columnsWithNoBeacons(row: Int): Set<Int> =
-  flatMapTo(mutableSetOf()) { s ->
-    s.columnsWithNoBeacons(row)
+@OptIn(ExperimentalStdlibApi::class)
+fun IntRange.exclude(r: IntRange): List<IntRange> {
+  if (this.isEmpty() || (r.first <= first && r.last >= last)) return emptyList()
+  if (r.isEmpty() || r.first > last || r.last < first) return listOf(this)
+  val ranges = mutableListOf<IntRange>()
+  if (first < r.first) ranges.add(first..<r.first)
+  if (last > r.last) ranges.add(r.last + 1..last)
+  return ranges
+}
+
+fun excludeColumnsWithNoBeacons(sensors: Iterable<Sensor>, row: Int, columns: IntRange): List<IntRange> =
+  sensors.fold(listOf(columns)) { ranges: List<IntRange>, sensor: Sensor ->
+    ranges.flatMap { it.exclude(sensor.columnsWithNoBeacons(row)) }
   }
 
 fun main() {
@@ -46,21 +53,30 @@ fun main() {
 
   fun part1(reader: Reader, y: Int): Int {
     val sensors = deploySensors(reader)
-    return (sensors
-      .flatMapTo(mutableSetOf()) { it.columnsWithNoBeacons(y) }
-      - sensors.flatMap { if (it.closestBeacon.y == y) setOf(it.closestBeacon.x) else emptySet() }.toSet()).size
+    val beaconsExcluded = sensors.flatMapTo(mutableSetOf()) { it.columnsWithNoBeacons(y) }
+    val knownBeacons = sensors.map { it.closestBeacon }.filter { it.y == y }.map { it.x }.toSet()
+    return (beaconsExcluded - knownBeacons).size
   }
 
-  fun part2(reader: Reader, searchSpace: IntRange): Int {
+  fun part2(reader: Reader, searchSpace: IntRange): Long {
     val sensors = deploySensors(reader)
     val beaconPosition = searchSpace.firstNotNullOf { y ->
-      println("Checking $y")
-      val x = (searchSpace - sensors.columnsWithNoBeacons(y)).firstOrNull()
-      x?.let { Coordinate(it, y) }
+      excludeColumnsWithNoBeacons(sensors, y, searchSpace)
+        .firstOrNull()
+        ?.let { Coordinate(it.first, y) }
     }
-    println("Found beacon at $beaconPosition")
-    return (beaconPosition.x * 4000000) + beaconPosition.y
+    return (beaconPosition.x * 4000000L) + beaconPosition.y
   }
+
+  assert((1..10).exclude(IntRange.EMPTY) == listOf(1..10))
+  assert(IntRange.EMPTY.exclude(1..10).isEmpty())
+  assert((1..10).exclude(11..20) == listOf(1..10))
+  assert((1..10).exclude(-1..11).isEmpty())
+  assert((1..10).exclude(3..8) == listOf(1..2, 9..10))
+  assert((1..10).exclude(1..10).isEmpty())
+  assert((1..10).exclude(-1..3) == listOf(4..10))
+  assert((1..10).exclude(8..13) == listOf(1..7))
+  assert((1..10).exclude(2..9) == listOf(1..1, 10..10))
 
   val testInput = """
     Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -80,8 +96,8 @@ fun main() {
   """.trimIndent()
 
   assert(part1(testInput.reader(), 10) == 26)
-  assert(part2(testInput.reader(), 0..20) == 56000011)
+  assert(part2(testInput.reader(), 0..20) == 56000011L)
 
-  execute("day15", "Part 1") { input -> part1(input, 2000000) }.let { assert(it == 5878678) }
-  execute("day15", "Part 2") { input -> part2(input, 0..4000000) } //.let { assert(it == 5878678) }
+  execute("day15", "Part 1") { input -> part1(input, 2000000) }.also { assert(it == 5878678) }
+  execute("day15", "Part 2") { input -> part2(input, 0..4000000) }.also { assert(it == 11796491041245L) }
 }
